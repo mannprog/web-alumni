680c1bec94df1772921f69d70e50fd50a680b429
Muh Hilman Sholehudin <manprog31@gmail.com>
2023-07-17 20:11:58 +0700

    Update UI Homepage

--- resources/views/home/welcome.blade.php
+++ resources/views/home/welcome.blade.php
@@ -14,7 +14,7 @@
                         laboris nisi ut aliquip ex ea commodo consequat.
                     </p>
 
-                    <div class="icon-box" data-aos="fade-up" data-aos-delay="100">
+                    {{-- <div class="icon-box" data-aos="fade-up" data-aos-delay="100">
                         <div class="icon"><i class="bi bi-briefcase"></i></div>
                         <h4 class="title"><a href="">Eiusmod Tempor</a></h4>
                         <p class="description">Et harum quidem rerum facilis est et expedita distinctio. Nam libero
@@ -33,7 +33,7 @@
                         <h4 class="title"><a href="">Dolor Sitema</a></h4>
                         <p class="description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut
                             aliquip ex ea commodo consequat tarad limino ata</p>
-                    </div>
+                    </div> --}}
 
                 </div>
 
@@ -82,13 +82,12 @@
         <div class="container">
             <div class="row" data-aos="zoom-in">
                 <div class="col-lg-9 text-center text-lg-start">
-                    <h3 class="cta-title">Call To Action</h3>
-                    <p class="cta-text"> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-                        dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                        culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <h3 class="cta-title">Ayo Bergabung, Eksplorasi Pengetahuan!</h3>
+                    <p class="cta-text">Jadilah bagian dari komunitas sekolah kami yang penuh semangat dan inspiratif.
+                        Segera bergabung dan capai prestasimu!</p>
                 </div>
                 <div class="col-lg-3 cta-btn-container text-center">
-                    <a class="cta-btn align-middle" href="#">Call To Action</a>
+                    <a class="cta-btn align-middle" href="#">Daftar Sekarang</a>
                 </div>
             </div>
 
@@ -180,73 +179,41 @@
         <div class="container">
             <div class="section-header">
                 <h3 class="section-title">Kontak Kami</h3>
-                <p class="section-description">Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                    accusantium doloremque</p>
+                <p class="section-description">Berikut adalah kontak kami yang dapat dihubungi.</p>
             </div>
         </div>
-
-        <!-- Uncomment below if you wan to use dynamic maps -->
-        <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d22864.11283411948!2d-73.96468908098944!3d40.630720240038435!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew+York%2C+NY%2C+USA!5e0!3m2!1sen!2sbg!4v1540447494452"
-            width="100%" height="380" frameborder="0" style="border:0" allowfullscreen></iframe>
-
         <div class="container mt-5">
-            <div class="row justify-content-center">
+            <div class="row">
+                <div class="col-lg-8">
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3959.9805483787104!2d107.76028151477338!3d-7.011570294935206!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68c1533c0f8d4d%3A0xff8005a5df42967!2sSMK%20PGRI%2035%20Solokan%20Jeruk!5e0!3m2!1sid!2sid!4v1689598314216!5m2!1sid!2sid"
+                        width="100%" height="380" frameborder="0" style="border:0;" allowfullscreen></iframe>
+                </div>
 
-                <div class="col-lg-3 col-md-4">
-
+                <div class="col-lg-4 px-3">
                     <div class="info">
                         <div>
                             <i class="bi bi-geo-alt"></i>
-                            <p>A108 Adam Street<br>New York, NY 535022</p>
+                            <p>Jl. R.H.O Kosasih No.90, Cibodas, <br> Kec. Solokanjeruk, Kabupaten Bandung, Jawa Barat 40376
+                            </p>
                         </div>
 
                         <div>
                             <i class="bi bi-envelope"></i>
-                            <p>info@example.com</p>
+                            <p>smkpgri35soljer@gmail.com</p>
                         </div>
 
                         <div>
-                            <i class="bi bi-phone"></i>
-                            <p>+1 5589 55488 55s</p>
+                            <i class="bi bi-telephone"></i>
+                            <p>(022) 85962018</p>
                         </div>
                     </div>
 
                     <div class="social-links">
+                        <a href="#" class="telephone"><i class="bi bi-phone"></i></a>
                         <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
                         <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
                         <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
-                        <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
-                        <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
-                    </div>
-
-                </div>
-
-                <div class="col-lg-5 col-md-8">
-                    <div class="form">
-                        <form action="forms/kontak.php" method="post" role="form" class="php-email-form">
-                            <div class="form-group">
-                                <input type="text" name="name" class="form-control" id="name"
-                                    placeholder="Your Name" required>
-                            </div>
-                            <div class="form-group mt-3">
-                                <input type="email" class="form-control" name="email" id="email"
-                                    placeholder="Your Email" required>
-                            </div>
-                            <div class="form-group mt-3">
-                                <input type="text" class="form-control" name="subject" id="subject"
-                                    placeholder="Subject" required>
-                            </div>
-                            <div class="form-group mt-3">
-                                <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
-                            </div>
-                            <div class="my-3">
-                                <div class="loading">Loading</div>
-                                <div class="error-message"></div>
-                                <div class="sent-message">Your message has been sent. Thank you!</div>
-                            </div>
-                            <div class="text-center"><button type="submit">Send Message</button></div>
-                        </form>
                     </div>
                 </div>
 
